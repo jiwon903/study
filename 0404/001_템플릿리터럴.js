48f07e37c9ffbcc1735e39b52af78238cdde0a9d
jiwon.An <wldnjs7152@naver.com>
2023-04-04 23:28:20 +0900

    수업진도 복습

---
+++ 0404/001_템플릿리터럴.js
@@ -0,0 +1,10 @@
+const x=10;
+const y =20;
+
+console.log(`x 값은 ${x} 이고 y 값은 ${y} 이고 두개를 곱한값은 ${x*y} 입니다.`);
+
+let tip1 = [1,2,3,4,5];
+let tip2=[...tip1]
+console.log(tip2.pop());
+console.log(tip2)
+console.log(tip1)
