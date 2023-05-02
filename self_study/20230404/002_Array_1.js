cbb7e5fec0ee98e883e643d0dc1284a77cfbc040
jiwon.An <wldnjs7152@naver.com>
2023-05-02 23:56:14 +0900

    복습

--- self_study/20230404/002_Array_1.js
+++ self_study/20230404/002_Array_1.js
@@ -1,24 +1,10 @@
-// arr(*****)
-const arr = [10, 20, 30]
-arr[0] = 100 // const는 값의 변화가 안된다고 하지 않았나? // 변경가능, mutable
-// arr = 100
-// arr = [10, 20, 30]
-console.log(arr)
-console.dir(arr) // 프러퍼티와 메서를 볼 수 있습니다.
+// 실무에서 사용하는 코드는 아닙니다.
+const arr = Array(100).fill(0)
+const arr2 = []
 
-// 문자열(string)
-const s = 'hello world'
-console.log(s[0])
-s[0] = 'i' // 불변, immutable
-console.log(s[0])
 
-// arr.1 // error
-// 숫자로 들어간 값은 대괄호로 호출할 수 있고
-// length는 문자입니다. 문자로 입력된 값은 .을찍어서 호출할 수 있습니다.
-arr[1]
-arr.length
-console.log('이거' + arr['length'])
-
-// 두개가 같은 얘기입니다.
-arr.length = 10
-arr[0] = 100
+// 같은 코드1 (이름이 없는 함수 사용)
+ arr.forEach(function(item, index){
+     arr2.push(index+1)
+ })
+console.log(arr2)
