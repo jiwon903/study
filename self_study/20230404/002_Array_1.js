4b9969a84527f8cd248f00d9ff6ee62ad1509e6f
jiwon.An <wldnjs7152@naver.com>
2023-05-04 10:36:11 +0900

    React 학습
    설치 및 구성 내용 살펴보기

--- self_study/20230404/002_Array_1.js
+++ self_study/20230404/002_Array_1.js
@@ -1,10 +1,10 @@
-// 실무에서 사용하는 코드는 아닙니다.
-const arr = Array(100).fill(0)
-const arr2 = []
+// map (면접 질문에서도 많이 나옵니다. 데이터를 뽑으려고 많이 사용합니다.)
+//map() 함수는 배열 내의 모든 요소 각각에 대하여 주어진 함수를 호출한 결과를 모아 새로운 배열을 반환하는 함수이다.
 
+// map은 forEach와 다르게 새로운 배열을 생성합니다.
 
-// 같은 코드1 (이름이 없는 함수 사용)
- arr.forEach(function(item, index){
-     arr2.push(index+1)
- })
-console.log(arr2)
+const arr = [1, 2, 3]
+const arr2 = arr.map(function (item, index) {
+    return item ** 2
+})
+console.log(arr2);
