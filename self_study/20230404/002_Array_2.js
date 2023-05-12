70359ea377c31120b1352e0099e914c698d621e4
jiwon.An <wldnjs7152@naver.com>
2023-05-12 22:57:32 +0900

    JS 복습

--- self_study/20230404/002_Array_2.js
+++ self_study/20230404/002_Array_2.js
@@ -1,8 +1,11 @@
-// reduce
+// reduce 함수를 알아보자 ! 
 const arr1 = [1, 2, 3, 4, 5]
 arr1.reduce((a, c) => a + c, 0)
 
 const arr1 = []
+arr1.reduce((a, c) => a + c)
+
+const arr1 = [1]
 arr1.reduce((a, c) => a + c)
 
 const arr1 = [1]
