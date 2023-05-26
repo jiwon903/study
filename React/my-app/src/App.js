1f9eb01e1f5055d0a278f4a870179926141990bc
jiwon.An <wldnjs7152@naver.com>
2023-05-26 22:45:55 +0900

    react 복습

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,28 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const date = new Date();
-  const name = '정대만!';
-  const someStyle = {backgroundColor:"yellow",color:"white"};
-  const yearStyle = {color:"red"};
-  let list = [
-    { no:1, area:"대전",visited: false},
-    { no:2, area:"부산",visited: true},
-    { no:3, area:"목포",visited: false}
-  ]
-  return (
-    <div>
-      <h1 style={someStyle}>안녕, {name} 1</h1>
-      <h2>안녕, 라이켓! 2</h2>
-      <div className="newClass"/>
-      <p style = {yearStyle}>년 : {date.getFullYear()}</p>
-      <p>월/일 : {date.getMonth() +1}/{date.getDate()}</p>
-      <p>시간 : {date.getHours()}시 {date.getMinutes()}분 {date.getSeconds()}초</p>
+let list = [
+  {no:1, area:'대전',visited:false},
+  {no:2, area:'부산',visited:true},
+  {no:3, area:'목포',visited:false},
+  {no:4, area:'제주도',visited:false},
+  {no:5, area:'경주',visited:true}
+];
 
-    </div>
-    
-  );
+function App() {
+  let areas = list.map((item,index)=> {
+    return (
+      <li key={index} className ={item.visited ? "list-area-item active" : "list-area-item"}>{item.area}</li>
+    )
+  })
+  return <ul className="list-area">{areas}</ul>
+  
 }
 
 export default App;
